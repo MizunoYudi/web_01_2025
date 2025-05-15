28e5f43ac1f65f88e4a6afb968cdbc3aab77cdee
MizunoYudi <joaomizunovy@gmail.com>
2025-05-15 20:32:11 -0300

    Parte Lab 13

---
+++ LAB_13_Exercicios_APIs/dist/app.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const jogador_1 = require("./jogador");
+const app = (0, express_1.default)();
+const PORT = process.env.PORT ?? 3000;
+app.use(express_1.default.json());
+const jogadores = [];
+function helloWorld(req, res) {
+    res.status(200).send('Teste');
+}
+function listarJogadores(req, res) {
+    let texto = '';
+    res.status(200).send(texto);
+}
+function filtrarPorId(req, res) {
+    try {
+        let id = req.params.id;
+        res.status(200).json({ ID: id });
+    }
+    catch (e) {
+        res.status(400).json({ Message: " Necessário informar o ID" });
+    }
+}
+function novoJogador(req, res) {
+    try {
+        let data = req.body;
+        if (!data.id || !data.nome || !data.cpf || !data.email || !data.telefone || !data.posicao) {
+            throw new Error("Falta informações.");
+        }
+        const j = new jogador_1.Jogador(data.id, data.nome, data.cpf, data.email, data.telefone, data.posicao);
+        jogadores.push(j);
+        res.status(200).json(j);
+    }
+    catch (e) {
+        res.status(400).json({ Message: e.message });
+    }
+}
+app.get('/api/jogadores/:id', filtrarPorId);
+app.post('/api/jogadores', novoJogador);
+app.listen(PORT, () => console.log(`API em execução na URL: http://localhost:${PORT}`));
---
+++ LAB_13_Exercicios_APIs/src/app.ts
@@ -0,0 +1,56 @@
+import express , { Request , Response } from "express"
+import { Jogador } from "./jogador"
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+app.use(express.json());
+
+const jogadores: Jogador[] = [];
+
+function helloWorld(req: Request, res: Response): void {
+    res.status(200).send('Teste');
+}
+
+function listarJogadores(req: Request, res: Response): void {
+    let texto: string = '';
+
+
+    res.status(200).send(texto);
+}
+
+function filtrarPorId(req: Request, res: Response): void {
+    try {
+        let id = req.params.id
+        for(let i = 0; i < jogadores.length; i++){
+            if(jogadores[i].id === 1){
+                
+            }
+        }
+        res.status(200).json({ID: id})
+    } catch (e: unknown) {
+        res.status(400).json({Message:" Necessário informar o ID"})
+    }
+}
+
+function novoJogador(req: Request, res: Response): void {
+    try {
+        let data: any = req.body;
+
+        if(!data.id || !data.nome || !data.cpf || !data.email || !data.telefone || !data.posicao){
+            throw new Error("Falta informações.");
+        }
+
+        const j = new Jogador(data.id, data.nome, data.cpf, data.email, data.telefone, data.posicao);
+        jogadores.push(j)
+        res.status(200).json(j)
+    } catch (e: unknown) {
+        res.status(400).json({Message:(e as Error).message})
+    }
+}
+
+app.get('/api/jogadores/:id', filtrarPorId);
+app.post('/api/jogadores', novoJogador);
+
+
+
+app.listen(PORT, () => console.log(`API em execução na URL: http://localhost:${PORT}`));
